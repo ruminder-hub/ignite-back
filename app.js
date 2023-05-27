c5cc7ba7a1c1bfba258722b1e3c3f075a36c0b2b
Ruminder Singh <ruminder.inbox@gmail.com>
2023-05-27 13:39:04 -0700

    Added home page json

--- app.js
+++ app.js
@@ -47,6 +47,7 @@ app.use(function(err, req, res, next) {
 });
 
 const port = process.env.PORT || 3000;
+console.log(`Port is ${port}`);
 //server port
 app.listen(port, () => {
   console.log(`Server is runing on port: ${port}`);
